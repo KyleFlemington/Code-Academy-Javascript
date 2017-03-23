efd1ebc2eedd59dab4090ad83fd59970a26a7a73
kyle <kyleflemington@gmail.com>
2017-03-22 18:18:48 -0700

    Lesson 1

--- CAJS.js
+++ CAJS.js
@@ -1,1 +1,155 @@
 /* https://www.codecademy.com/KyleFlemington */
+
+/* 1.1 What is your name  */
+"Kyle"
+
+/* 1.2 Discover the length */
+"kyle".length
+
+
+/* 1.3 Basic Math */
+
+5 + 7 
+
+/* 1.4 Numbers and more */
+
+6 * 6
+
+/* 1.5  Error: does not compute! */
+rumpelstiltskin
+
+/* 1.6 Editor and comments */
+
+//Using Git to illustrate JS Skills
+"cake".length
+
+/* 1.7 What am I Learning? */
+confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
+
+/* 1.8 Interactive Javascript */
+//Notation
+confirm("I like cats")
+
+
+/* 1.9 What is programming? */
+prompt("Where are you from?")
+
+
+/* 1.10 */
+"This is Sparta".lenth;
+
+
+/* 1.11 Data Type III: Booleans */
+"I'm coding like a champ".length >= 10
+
+/* 1.12 Using console.log */
+console.log(2*5)
+console.log("Hello")
+
+
+/* 1.13 Comparisions */
+// Here is an example of using the greater than (>) operator.
+console.log(15 > 4); // 15 > 4 evaluates to true, so true is printed.
+
+// Fill in with >, <, === so that the following print out true:
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length >  8);
+console.log(8*2 == 16);
+
+
+/* 1.14 Decisions, decisions */
+if ("kyle".length <= 7) {
+	console.log("True")
+};
+
+
+/* 1.15 Computers are smart */
+if ("Steve".length > 100) {
+	console.log("Steve! That's not right")
+} else {
+	console.log("Pretty sneaky Steve")
+}
+
+/* 1.16 More Practice with conditionals */
+if ("kyle".length == 5) {
+  console.log("The condition is true");
+} else  {
+  console.log("The condition is false");
+}
+
+/* 1.17 Computer aren't that smart */
+
+if (10 == 10) {
+  console.log("You got a true!");
+} else {
+  console.log("You got a false!");
+}
+
+
+/* 1.18 Mid-lesson breather */
+if (12 / 4 === "Ari".length) {
+  confirm("Will this run the first block?");
+} else {
+  confirm("Or the second block?");
+}
+
+
+/* 1.19 Math */
+
+if ("jon".length * 2 / (2+1) === 2) {
+	console.log("The answer makes sense!")
+} else {
+	console.log("Error Error Error")
+}
+
+
+/* 1.20 Math and the Modulo */
+console.log( 14 % 3 );
+console.log( 99 % 8 );
+console.log( 11 % 3 );
+
+
+/* 1.21 Modulo and if/else */
+if( 4 % 4 === 0 ) {
+    console.log("The first number is even");
+} else {
+    console.log("The first number is odd");
+}
+
+/* 1.22 Substrings */
+"wonderful day".substring(3,7);
+
+/* 1.23 More substring practice */
+console.log("January".substring(0,3));
+console.log("Melbourne is great".substring(0,12));
+console.log("Hamburgers".substring(3,10));
+
+
+/* 1.24 Variables */
+myAge = 27;
+console.log(myAge)
+
+
+/* 1.25 More Variable Practice */
+// Declare a variable on line 3 called
+// myCountry and give it a string value.
+myCountry = "Canada";
+
+// Use console.log to print out the length of the variable myCountry.
+console.log(myCountry.length);
+
+// Use console.log to print out the first three letters of myCountry.
+console.log(myCountry.substring(0,3));
+
+
+/* 1.26 Change Variable Values */
+// On line 2, declare a variable myName and give it your name.
+var myName = "Kyle"
+// On line 4, use console.log to print out the myName variable.
+console.log(myName);
+// On line 7, change the value of myName to be just the first 2 
+// letters of your name.
+myName = "Ky"
+// On line 9, use console.log to print out the myName variable.
+console.log(myName);
+
